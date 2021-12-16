1585c07ee0b61cee216b60329139d8443bd37907
onticinc <onticinc@gmail.com>
2021-12-16 10:28:54 -0700

    working on templates

---
+++ react-bulma/src/bulma/Absurd.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+
+class Absurd extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colored-dna-back" className="hero is-white is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns  is-vcentered reverse-columns">
+                <div className="column
+          is-10-mobile 
+          is-10-tablet 
+          is-5-desktop 
+          is-5-widescreen 
+          is-5-fullhd " data-aos="fade-down">
+                  <h1 className="title titled is-1 mb-6">
+                    Evolving business with technology
+                  </h1>
+                  <div className="buttons">
+                    <button className="button is-yellow">Esablished technical teams</button>
+                    <button className="button">Less-technical teams</button>
+                  </div>
+                </div>
+                <div data-aos="fade-right" className="column
+          is-10-mobile 
+          is-10-tablet 
+          is-4-desktop 
+          is-7-widescreen 
+          is-4-fullhd is-offset-1-fullhd">
+                  <figure className="image is-square">
+                    <img src="../images/people-solving-a-puzzle.png" alt="people-solving-puzzle" />
+                  </figure>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        <section className="hero is-white is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns  is-vcentered">
+                <div data-aos="fade-left" className="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-4-desktop is-offset-1-desktop
+          is-4-widescreen is-offset-1-widescreen
+          is-4-fullhd is-offset-1-fullhd">
+                  <figure className="image is-square">
+                    <img src="../images/undraw_Camera_re_cnp4.svg" alt="camera" />
+                  </figure>
+                </div>
+                <div data-aos="fade-down" className="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-5-desktop is-offset-1-desktop
+          is-5-widescreen is-offset-1-widescreen
+          is-5-fullhd is-offset-1-fullhd">
+                  <h1 className="titled title is-1 mb-6">
+                    absurd illustrations that make sense
+                  </h1>
+                  <h2 className="subtitled subtitle">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+                    nesciunt totam magni quas.
+                  </h2>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="hero is-white is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns  is-vcentered reverse-columns">
+                <div data-aos="fade-right" className="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-5-desktop is-offset-1-desktop
+          is-5-widescreen is-offset-1-widescreen
+          is-5-fullhd is-offset-1-fullhd">
+                  <h1 className="title titled is-1 mb-6">
+                    absurd illustrations that make sense
+                  </h1>
+                  <h2 className="subtitle subtitled">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+                    nesciunt totam magni quas.
+                  </h2>
+                </div>
+                <div data-aos="fade-down" className="column
+          is-10-mobile is-offset-1-mobile
+          is-10-tablet is-offset-1-tablet
+          is-4-desktop is-offset-1-desktop
+          is-4-widescreen is-offset-1-widescreen
+          is-4-fullhd is-offset-1-fullhd" data-aos="fade-up">
+                  <figure className="image is-square">
+                    <img src="../images/undraw_Camera_re_cnp4.svg" alt="camera-2" />
+                  </figure>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="hero is-medium has-text-centered">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns is-centered">
+                <div data-aos="zoom-in-up" className="column is-8">
+                  <h1 className="title titled is-1 mb-6">
+                    Primary bold title <span id="typewriter"></span>
+                  </h1>
+                  <h2 className="subtitle subtitled">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
+                    nesciunt totam magni quas.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  </h2>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Absurd;

cf3f6be332fb002e72e3a33d6266d4d630c227d8
onticinc <onticinc@gmail.com>
2021-12-16 00:15:56 -0700

    figuring it out

--- react-bulma/public/bulma/react-templates/cover.js
+++ react-bulma/public/bulma/react-templates/cover.js
@@ -9,7 +9,7 @@ class Cover extends Component {
                         <div className="container">
                             <div className="navbar-brand">
                                 <a className="navbar-item" href="../">
-                                    <img src="../images/bulma.png" alt="Logo" />
+                                    <img src="../images/bulma.png" alt="Logo">
                                 </a>
                                 <span className="navbar-burger burger" data-target="navbarMenu">
                                     <span></span>
@@ -21,21 +21,11 @@ class Cover extends Component {
                                 <div className="navbar-end">
                                     <div className="tabs is-right">
                                         <ul>
-                                            <li className="is-active">
-                                                <a>Home</a>
-                                            </li>
-                                            <li>
-                                                <a href="">Examples</a>
-                                            </li>
-                                            <li>
-                                                <a href="">Features</a>
-                                            </li>
-                                            <li>
-                                                <a href="">Team</a>
-                                            </li>
-                                            <li>
-                                                <a href="">Help</a>
-                                            </li>
+                                            <li className="is-active"><a>Home</a></li>
+                                            <li><a href="">Examples</a></li>
+                                            <li><a href="">Features</a></li>
+                                            <li><a href="">Team</a></li>
+                                            <li><a href="">Help</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -48,23 +38,22 @@ class Cover extends Component {
                         <div className="columns is-vcentered">
                             <div className="column is-5">
                                 <figure className="image is-4by3">
-                                    <img
-                                        src="https://picsum.photos/800/600/?random"
-                                        alt="Description"
-                                    />
+                                    <img src="https://picsum.photos/800/600/?random" alt="Description">
                                 </figure>
                             </div>
                             <div className="column is-6 is-offset-1">
-                                <h1 className="title is-2">Superhero Scaffolding</h1>
+                                <h1 className="title is-2">
+                                    Superhero Scaffolding
+                                </h1>
                                 <h2 className="subtitle is-4">
                                     Let this cover page describe a product or service.
                                 </h2>
-                                <br />
-                                <p className="has-text-centered">
-                                    <a className="button is-medium is-info is-outlined">
-                                        Learn more
-                                    </a>
-                                </p>
+                                <br>
+                                    <p className="has-text-centered">
+                                        <a className="button is-medium is-info is-outlined">
+                                            Learn more
+                                        </a>
+                                    </p>
                             </div>
                         </div>
                     </div>
@@ -74,9 +63,7 @@ class Cover extends Component {
                     <div className="container">
                         <div className="tabs is-centered">
                             <ul>
-                                <li>
-                                    <a>And this is the bottom</a>
-                                </li>
+                                <li><a>And this is the bottom</a></li>
                             </ul>
                         </div>
                     </div>

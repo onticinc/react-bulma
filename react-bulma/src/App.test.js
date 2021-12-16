01a108541845397aa2dd2be17264408e31b98cb3
onticinc <onticinc@gmail.com>
2021-12-16 14:41:38 -0700

    updated tests

--- react-bulma/src/App.test.js
+++ react-bulma/src/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Forum from './Forum';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Dashboard tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Dashboard/i);
+  expect(spanElement).toBeInTheDocument();
 });
+
+test('Customer tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Customers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('Authentication tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Authentication/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('Payments tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Payments/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('Transfer tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Transfers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+
+
+

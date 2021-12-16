3c37d7c663e7730e53704a99d2a4d0c451b8cb7f
onticinc <onticinc@gmail.com>
2021-12-16 11:40:11 -0700

    updated forum template

--- react-bulma/src/App.js
+++ react-bulma/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Absurd from './Absurd';
+import Forum from './Forum';
 
 function App() {
   return (
     <div className="App">
-      <Absurd />
+      {/* <Absurd /> */}
+      <Forum />
 
     </div>
   );

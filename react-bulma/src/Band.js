608c08e80c14a1ac77cb0ed08d388708d4b34dba
onticinc <onticinc@gmail.com>
2021-12-17 10:35:38 -0700

    random push

--- react-bulma/src/Band.js
+++ react-bulma/src/Band.js
@@ -7,7 +7,6 @@ const comment = {
     comment: "",
 
 };
-
 const commentList = [
     {
         name: "I love bball....",
@@ -30,6 +29,33 @@ const commentList = [
         author: "@tombrady",
     },
 ];
+
+const displayCommentList = commentList.map((c, idx) => {
+    return (
+        <div className="post" key={idx}>
+            <div className="card-image">
+                <figure className="image is-2by1">
+                    <img src="https://images.pexels.com/photos/167635/pexels-photo-167635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Placeholder image" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">John Smith</p>
+                        <p className="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    <a>@bulmaio</a>.
+                    <a href="#">#css</a>
+                    <a href="#">#responsive</a>
+                </div>
+            </div>
+        </div>  
+    );
+});
 
 class Band extends Component {
 
